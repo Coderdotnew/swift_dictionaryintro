1018389800c9ef2191212a6171e27868685b077a
Stephanholly <sholly@usc.edu>
2016-10-27 14:19:23 -0700

    first commit

---
+++ dictionaryintro.swift
@@ -0,0 +1,33 @@
+// DO NOT ALTER THE FOLLOWING Dictionary
+var mascots: [String: String] = [
+  "stanford" : "Tree",
+  "asu" : "Sun Devil",
+  "uofa" : "Wildcat",
+  "oregon" : "Bird",
+  "usc: : "Trojan"
+]
+
+// return the stanford mascot
+
+
+
+
+
+// update the oregon mascot to be "Duck"
+
+
+
+
+
+// return usc key symbol
+
+
+
+
+
+// add a key for ucla
+
+
+
+
+// add a mascot for ucla
